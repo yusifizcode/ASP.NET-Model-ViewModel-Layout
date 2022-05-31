6578d0e0984121947d2ff1d974b365236ecdbdb0
yusifizcode <5ba0gh2@code.edu.az>
2022-05-31 13:33:39 +0400

    Eterna Site

--- EternaSite/Controllers/HomeController.cs
+++ EternaSite/Controllers/HomeController.cs
@@ -112,6 +112,58 @@ namespace EternaSite.Controllers
             return View(services);
         }
 
+        public IActionResult ServiceDetail(int id)
+        {
+            List<Service> services = new List<Service>
+            {
+                new Service
+                {
+                    Id = 1,
+                    Icon = "bx bxl-dribbble",
+                    Title = "Lorem Ipsum",
+                    Content = "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"
+                },
+                new Service
+                {
+                    Id = 2,
+                    Icon = "bx bx-file",
+                    Title = "Dolor Sitema",
+                    Content = "Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata"
+                },
+                new Service
+                {
+                    Id = 3,
+                    Icon = "bx bx-tachometer",
+                    Title = "Sed ut perspiciatis",
+                    Content = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+                },
+                new Service
+                {
+                    Id = 4,
+                    Icon = "bx bx-world",
+                    Title = "Nemo Enim",
+                    Content = "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"
+                },
+                new Service
+                {
+                    Id = 5,
+                    Icon = "bx bx-slideshow",
+                    Title = "Dele cardo",
+                    Content = "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"
+                },
+                new Service
+                {
+                    Id = 6,
+                    Icon = "bx bx-arch",
+                    Title = "Divera don",
+                    Content = "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"
+                },
+            };
+
+            Service service = services.Find(x=>x.Id == id);
+            return View(service);
+        }
+
         public IActionResult Team()
         {
             return View();
